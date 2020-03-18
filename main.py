23ef30c8bc88c3514777d3fe073021694bc93ff4
AlexA <AlexAntlex@yandex.ru>
2020-03-18 13:18:03 +0300

    add slideshow

--- main.py
+++ main.py
@@ -126,5 +126,61 @@ def upload_file():
             </html>'''
 
 
+@app.route('/landscape')
+def landscape():
+    return f'''<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <title>Пейзажи Марса</title>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link rel="stylesheet" 
+                        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+                    <link rel="stylesheet" type="text/css"
+                        href="{url_for('static', filename='css/style.css')}"/>
+                    <script 
+                        src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                    <script 
+                        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+                    <script 
+                        src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+                </head>
+                <body>
+                    <div class="container mt-3">
+                        <div class="text-center">
+                            <h1>Пейзажи Марса</h1>
+                        </div>
+                        <div id="Mars" class="carousel slide" data-ride="carousel">
+                            <ul class="carousel-indicators">
+                                <li data-target="#Mars" data-slide-to="0" class="active"></li>
+                                <li data-target="#Mars" data-slide-to="1"></li>
+                                <li data-target="#Mars" data-slide-to="2"></li>
+                            </ul>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="{url_for('static', filename='img/mars_1.jpg')}" 
+                                        alt="Mars_1" width="570" height="500">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="{url_for('static', filename='img/mars_2.jpg')}" 
+                                    alt="Mars_2" width="570" height="500">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="{url_for('static', filename='img/mars_3.jpg')}" 
+                                    alt="Mars_3" width="570" height="500">
+                                </div>
+                              </div>
+                              <a class="carousel-control-prev" href="#Mars" data-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                              </a>
+                              <a class="carousel-control-next" href="#Mars" data-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                              </a>
+                        </div>
+                    </div>     
+                </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
